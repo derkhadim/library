ce4a0569a182706f50bd21c1eee96fa50f688a21
derkhadim <dermouhamadou@gmail.com>
2016-11-27 17:32:22 +0000

    Component for navbar

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -1,17 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
+  model: function () {
     return this.store.createRecord('library');
   },
+
+  setupController: function (controller, model) {
+    this._super(controller, model);
+
+    controller.set('title', 'Create a new library');
+    controller.set('buttonLabel', 'Create');
+  },
+
+  renderTemplate() {
+    this.render('libraries/form');
+  },
+
   actions: {
-    saveLibrary(newLabrary) {
-      newLabrary.save().then(() => this.TransitionTo('libraries'));
+
+    saveLibrary(newLibrary) {
+      newLibrary.save().then(() => this.transitionTo('libraries'));
     },
+
     willTransition() {
       let model = this.controller.get('model');
 
-      if(model.get('isNew')) {
+      if (model.get('isNew')) {
         model.destroyRecord();
       }
     }
